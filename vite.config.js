9c6d458da6c8a11a48ac24640982b56b314b5f03
Sagar Maurya <sagarmaurya010@gmail.com>
2026-03-07 08:13:00 +0530

    Update portfolio

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // This 'base' path is mandatory for GitHub Pages to find your files
-  base: '/', 
+  base: '/portfolio/', // Add this line!
 })
